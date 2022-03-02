60a8054c5c08797f4e8029b3d66d08624fa7262a
Chisom Chukwuma <chukwumachisom18@gmail.com>
2022-03-02 02:18:14 -1100

    An advice generator app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import './style/All.css'
+import patternMobile from './assets/pattern-divider-mobile.svg'
+import patternDesktop from './assets/pattern-divider-desktop.svg'
+import dice from './assets/icon-dice.svg'
+const App = () => {
+  const [advice, setAdvice] = useState([])
+  const [loading, setLoading] = useState(true)
 
-function App() {
+  const getAdvice = () => {
+    axios.get('https://api.adviceslip.com/advice').then((response) => {
+      console.log(response)
+      const myAdvice = response.data
+      setAdvice(myAdvice)
+      setLoading(false)
+      console.log(advice);
+    })
+  }
+  useEffect(() => {
+    getAdvice()
+  }, [])
+  // console.log(advice.slip.id);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {loading === false && (
+        <Fragment>
+          <div className='small'>
+            <small> advice #{advice.slip.id}</small>
+          </div>
+          <div className='lorem'>
+            <p>
+              {advice.slip.advice}
+            </p>
+          </div>
+          <div className='pause'>
+            <source media='(min-width:768px)' srcSet={patternDesktop} />
+            <img src={patternMobile} alt='divider' />
+          </div>
+          <div className='dice'>
+            <img onClick={getAdvice} src={dice} alt='Dice' />
+          </div>
+        </Fragment>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
